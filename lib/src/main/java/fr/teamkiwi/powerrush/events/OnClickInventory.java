9df524c5c656255db58d21a23203414b2a4e5a8c
Falkunai <falkunai.gaming@gmail.com>
2023-07-07 19:32:19 +0200

    Début de la création du OnClickInventory n°2

--- lib/src/main/java/fr/teamkiwi/powerrush/events/OnClickInventory.java
+++ lib/src/main/java/fr/teamkiwi/powerrush/events/OnClickInventory.java
@@ -15,6 +15,8 @@ import org.bukkit.inventory.ItemStack;
 
 public class OnClickInventory implements Listener {
 
+    String consoleSender = "" + ChatColor.GRAY + ChatColor.ITALIC + "[" + ChatColor.DARK_GREEN + "POWER RUSH" + ChatColor.GRAY + "]";
+
     @EventHandler
     public void onClickInConfig(InventoryClickEvent event){
 
@@ -22,24 +24,26 @@ public class OnClickInventory implements Listener {
         ItemStack clickedItem = event.getCurrentItem();
         HumanEntity player = event.getWhoClicked();
 
+        ItemStack acaciaFence = new ItemStack(Material.ACACIA_FENCE);
+        ItemStack cobblestoneWall = new ItemStack(Material.COBBLE_WALL);
+        ItemStack acaciaFenceGate = new ItemStack(Material.ACACIA_FENCE_GATE);
+
+        WorldBorder wb = Bukkit.getWorld("world").getWorldBorder();
+        wb.setCenter(0, 0);
+
         if (clickedInventory.getTitle().equals(ChatColor.DARK_PURPLE + "Config Menu")){
 
-            switch (clickedItem.getType()){
+            switch (clickedItem.getType()) {
 
                 //Config de bordure
                 case BARRIER :
-                    WorldBorder wb = Bukkit.getWorld("world").getWorldBorder();
 
                     Inventory border = Bukkit.createInventory(null, 9*1, ChatColor.AQUA + "Border Menu");
                     ItemStack[] borderList = new ItemStack[9*1];
 
-                    ItemStack redClay = new ItemStack(Material.CLAY, 1, (byte)14);
-                    ItemStack lightGrayClay = new ItemStack(Material.CLAY, 1, (byte)8);
-                    ItemStack greenClay = new ItemStack(Material.CLAY, 1, (byte)5);
-
-                    borderList[3] = redClay;
-                    borderList[5] = lightGrayClay;
-                    borderList[7] = greenClay;
+                    borderList[3] = acaciaFence;
+                    borderList[5] = cobblestoneWall;
+                    borderList[7] = acaciaFenceGate;
 
                     player.openInventory(border);
                     break;
@@ -66,6 +70,38 @@ public class OnClickInventory implements Listener {
             }
 
             event.setCancelled(true);
+
+        }
+
+        if (clickedInventory.getTitle().equals(ChatColor.AQUA + "Border Menu")){
+
+            switch (clickedItem.getType()) {
+
+                case ACACIA_FENCE :
+
+                    wb.setSize(wb.getSize() - 100);
+                    player.sendMessage(consoleSender + "La taille de la bordure a été réduite de " + ChatColor.RED + "50 blocs");
+
+                    break;
+
+                case COBBLE_WALL :
+
+                    wb.setSize(1000);
+                    player.sendMessage(consoleSender + "La taille de la bordure a été réinitialisée à " + ChatColor.AQUA + "500 blocs");
+
+                    break;
+
+                case ACACIA_FENCE_GATE :
+
+                    wb.setSize(wb.getSize() + 100);
+                    player.sendMessage(consoleSender + "La taille de la bordure a été augmentée de " + ChatColor.GREEN + "50 blocs");
+
+                    break;
+
+                default:
+                    break;
+
+            }
 
         }
 
