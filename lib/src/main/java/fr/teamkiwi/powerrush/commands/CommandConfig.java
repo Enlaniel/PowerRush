a3a7055fadaa05f44574b9137808a0174de2d9f5
Enlaniel <augustin@desnot.com>
2023-07-07 16:31:05 +0200

    aaaaaahhhhhh

--- lib/src/main/java/fr/teamkiwi/powerrush/commands/CommandConfig.java
+++ lib/src/main/java/fr/teamkiwi/powerrush/commands/CommandConfig.java
@@ -3,13 +3,17 @@ package fr.teamkiwi.powerrush.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class CommandConfig implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		
-		
+		if(sender instanceof Player player) {
+			
+			
+		}
 		
 		
 		return false;
